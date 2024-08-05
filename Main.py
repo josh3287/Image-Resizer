8ca565d2d983f4b237053346f68f3a75d792e062
josh3287 <91765645+josh3287@users.noreply.github.com>
2024-08-05 11:08:49 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,3 +8,8 @@ from PIL import Image
 image = Image.open('AUTUMN.jpeg')
 
 print(f"Current size: {image.size}")
+
+
+resized_image = image.resize((500, 500))
+
+resized_image.save('Pythonchngedsize.jpeg')
